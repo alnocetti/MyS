9f62aba2a51109f58ac56c8d17b66606bcd377bb
anocetti <alnocetti@gmail.com>
2019-06-07 17:06:37 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import d3 from "d3";
+
+class Grafico{
+
+    draw_graph = function() {
+        instance.options.xAxis.domain[0] = parseInt(document.getElementById("a").value);
+        instance.options.xAxis.domain[1] = parseInt(document.getElementById("b").value);
+        if (document.getElementById("fx").value !== ""){
+            instance.options.data[0].fn = document.getElementById("fx").value;
+        }
+        // var ejeX = [parseInt(document.getElementById("a").value), parseInt(document.getElementById("b").value)];
+
+        // console.log(ejeX);
+        instance.draw();
+        // console.log(document.getElementById("fx").value)
+        // console.log(instance.options.data[0].fn);
+    }
+
+}
+window.d3 = d3;
+
+const functionPlot = require("function-plot");
+
+const graph = document.querySelector("#graph");
+
+var instance = functionPlot({
+  target: graph,
+  yAxis: { domain: [-1, 9] },
+  tip: {
+    renderer: function() {}
+  },
+  grid: true,
+  data: [
+    {
+      fn: "x^3",
+    //   derivative: {
+    //     fn: "2 * x",
+    //     updateOnMouseMove: true
+    //   }
+    },
+    {
+        fnType: "implicit",
+        color: "green",
+        fn: "x = 3 + 0*y"
+        // fnType: "points",
+        // color: "red",
+        // points: "0,1"
+    },
+  ],
+});
+
+var grafico = new Grafico();
+document.getElementById("btn").addEventListener("click" , grafico.draw_graph);
