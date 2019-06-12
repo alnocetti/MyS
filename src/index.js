f21c95a1964267f6be558834c7251cb6db7aac62
alnocetti <alnocetti@gmail.com>
2019-06-12 09:26:46 -0300

    testing

--- src/index.js
+++ src/index.js
@@ -15,11 +15,9 @@ import d3 from "d3";
 
 class Grafico{
 
-
-
     draw_graph = function() {
-        var xMin = parseInt(document.getElementById("a").value);
-        var xMax = parseInt(document.getElementById("b").value);
+         var xMin = parseInt(document.getElementById("a").value);
+         var xMax = parseInt(document.getElementById("b").value);
 
         instance.options.data[0].range[0] = xMin;
         instance.options.data[0].range[1] = xMax;
@@ -27,22 +25,42 @@ class Grafico{
             instance.options.data[0].fn = document.getElementById("fx").value;
         }
         instance.draw();
+
         
     }
 
     genRandomPoints = function(){
+      var xMin = parseInt(document.getElementById("a").value);
+      var xMax = parseInt(document.getElementById("b").value);
 
-      this.getMax();
+      var yMax = this.getMax;
+      var yMin = this.getMin;
+      console.log('y max: ' + yMax);
+      console.log('y min: ' + yMin);
+
+
+      var shots = parseInt(document.getElementById("shots").value);
+
+      instance.options.data[1].fnType = 'points';
+      instance.options.data[1].graphType = 'scatter';
+
+      for(var shot = 0; shot <= shots; shot ++){
+
+        var xPoint = Math.random() * (+xMax - +xMin) + +xMin;
+        var yPoint = Math.random() * (+yMax - +yMin) + +yMin;
+
+        instance.options.data[1].points.push([xPoint,yPoint]); 
+        console.log('x: ' + xPoint  + 'y: ' + yPoint);
+      }
 
     }
     
     getMax = function(){
       var xMin = parseInt(document.getElementById("a").value);
       var xMax = parseInt(document.getElementById("b").value);     
-      var shots = parseInt(document.getElementById("shots").value);
       var yMax = 0;
 
-      for(var shot = 1; shot <= shots; shot ++){
+      for(var i = 1; i <= xMax; i ++){
 
         var xToEvaluate = Math.random() * (+xMax - +xMin) + +xMin;
   
@@ -59,16 +77,17 @@ class Grafico{
       }
       console.log('Max found: ' + yMax);
       console.log('New max: ' + yMax * 1.20);
+      return yMax;
 
     }
     
     getMin = function(){
       var xMin = parseInt(document.getElementById("a").value);
       var xMax = parseInt(document.getElementById("b").value);     
-      var shots = parseInt(document.getElementById("shots").value);
+      // var shots = parseInt(document.getElementById("shots").value);
       var yMin = 0;
 
-      for(var shot = 1; shot <= shots; shot ++){
+      for(var i = 1; i <= xMax; i ++){
 
         var xToEvaluate = Math.random() * (+xMax - +xMin) + +xMin;
   
@@ -115,11 +134,11 @@ var instance = functionPlot({
     },
     {
         points: [
-          [1, 1],
-          [2, 1],
-          [2, 2],
-          [1, 2],
-          [1, 1]
+          // [1, 1],
+          // [2, 1],
+          // [2, 2],
+          // [1, 2],
+          // [1, 1]
         ],
         fnType: 'points',
         graphType: 'scatter'
@@ -136,6 +155,7 @@ var instance = functionPlot({
 });
 
 var grafico = new Grafico();
+document.getElementById("btn").addEventListener("click" , grafico.genRandomPoints);
 document.getElementById("btn").addEventListener("click" , grafico.draw_graph);
 
 document.getElementById("btn").addEventListener("click" , grafico.getMax);
